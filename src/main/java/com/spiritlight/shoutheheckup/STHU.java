680c00eeb4b68826922ee97dfda2e1880c4e8817
Ori <38721999+OriSpirit@users.noreply.github.com>
2022-05-13 14:28:33 +0800

    1.2 - Made text block able to block phrases (Untested)

--- src/main/java/com/spiritlight/shoutheheckup/STHU.java
+++ src/main/java/com/spiritlight/shoutheheckup/STHU.java
@@ -14,12 +14,12 @@ public class STHU
 {
     public static final String MODID = "sthu";
     public static final String NAME = "Shout The Heck Up!";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
     public static String prefix = "§6[§rSTHU§6]";
     public static boolean active = true;
     public static ArrayList<String> players = new ArrayList<String>();
     public static ArrayList<String> bannedWords = new ArrayList<String>();
-    public static String version = "1.1";
+    public static String version = "1.2";
     public static int hidelevel = 0;
     @EventHandler
     public void init(FMLInitializationEvent event) throws IOException
